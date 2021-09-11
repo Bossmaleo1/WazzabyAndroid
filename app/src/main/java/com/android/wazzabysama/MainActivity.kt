b41996fabfb4f63f201916264a94a85614b9c039
sidney <sidneymaleoregis@gmail.com>
2021-09-11 04:12:24 +0200

    our launch view

--- app/src/main/java/com/android/wazzabysama/MainActivity.kt
+++ app/src/main/java/com/android/wazzabysama/MainActivity.kt
@@ -6,8 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.android.wazzabysama.ui.theme.WazzabySamaTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,22 +15,9 @@ class MainActivity : ComponentActivity() {
             WazzabySamaTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    LaunchView()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WazzabySamaTheme {
-        Greeting("Android")
     }
 }
