342e94a7ea1cb796df5a0a35c13d7a03dbe9b1af
sidney <sidneymaleoregis@gmail.com>
2022-08-18 02:57:08 +0200

    We upgrade our dark mode ui

--- app/src/main/java/com/android/wazzabysama/ui/views/bottomnavigationviews/privatemessage/PrivateMessageView.kt
+++ app/src/main/java/com/android/wazzabysama/ui/views/bottomnavigationviews/privatemessage/PrivateMessageView.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CornerSize
@@ -15,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -27,7 +29,7 @@ import com.android.wazzabysama.ui.components.WazzabyDrawerDestinations
 @ExperimentalMaterial3Api
 @Composable
 fun PrivateMessageView(navController: NavHostController) {
-    // .padding(top = 2.5.dp, bottom = 2.5.dp, end = 5.dp, start = 5.dp)
+    val isDark = isSystemInDarkTheme()
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -44,7 +46,7 @@ fun PrivateMessageView(navController: NavHostController) {
             horizontalArrangement = Arrangement.Start
         ) {
             Image(
-                painter = painterResource(id = R.drawable.ic_profile_colorier),
+                painter = painterResource(id = R.drawable.ic_profile),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
@@ -56,18 +58,23 @@ fun PrivateMessageView(navController: NavHostController) {
             Column(modifier = Modifier.padding(4.dp)) {
                 Text(
                     text = "Sidney MALEO",
-                    style = MaterialTheme.typography.titleMedium
+                    modifier = Modifier.padding(4.dp, 0.dp,0.dp,0.dp),
+                    style = MaterialTheme.typography.titleMedium,
+                    color = if (!isDark) { colorResource(R.color.black40) } else { Color.White  },
+                    maxLines = 1
                 )
                 Text(
-                    text = "Et maintenant il parle lui même de venir chez ...",
+                    text = "Et maintenant il parle lui même de venir chez maleo-sama regis maleo",
                     modifier = Modifier.padding(4.dp, 0.dp,0.dp,0.dp),
-                    style = MaterialTheme.typography.titleSmall
+                    style = MaterialTheme.typography.titleSmall,
+                    color = if (!isDark) { colorResource(R.color.black40) } else { Color.White  },
+                    maxLines = 1
                 )
             }
         }
 
     }
-    Divider(color = MaterialTheme.colorScheme.onPrimary)
+    Divider(color = if(!isDark) { MaterialTheme.colorScheme.onPrimary } else { Color.White})
 
 }
 
